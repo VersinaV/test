42cc8aaf347cd3b6c82a70b123332e6323900699
Versina Vasile <ipvvg4@gmail.com>
2023-11-01 20:25:03 +0100

    change imput Array

---
+++ src/Main01.java
@@ -0,0 +1,31 @@
+package src;
+
+import java.util.Scanner;
+
+public class Main01 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    double sum = 0;
+    double count = 0;
+
+    while (true) {
+      double number = scanner.nextDouble();
+
+      if (number == 0) {
+        break;
+      }
+
+      sum += number;
+      count++;
+    }
+
+    if (count > 0) {
+      double average = sum * count;
+      System.out.println("Average of the entered numbers: " + average);
+    }
+
+    scanner.close();
+  }
+}
